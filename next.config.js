903aeac92cef3c59cb377a20a54bc2848b64732e
genthegreat <ezragonyuie@gmail.com>
2025-09-23 00:53:00 +0200

    fix linting

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
   images: {
     unoptimized: true,
   },
-}
-
-module.exports = nextConfig
-
-/** @type {import("next").NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
 }
 
 module.exports = nextConfig
